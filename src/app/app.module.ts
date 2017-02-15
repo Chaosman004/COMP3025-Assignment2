05bdac20fac23718808a8a8bc1291599f3f5fe11
Chaosman004 <mr.robert.page@gmail.com>
2017-02-15 14:40:53 -0500

    daatabase connection established some extra pages done too

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,19 @@ import { AboutPage } from '../pages/about/about';
 import { ContactPage } from '../pages/contact/contact';
 import { HomePage } from '../pages/home/home';
 import { TabsPage } from '../pages/tabs/tabs';
+import { DetailsPage } from '../pages/details/details';
+
+// Import the AF2 Module
+import { AngularFireModule } from 'angularfire2';
+
+// AF2 Settings
+export const firebaseConfig = {
+    apiKey: "AIzaSyDtyhXodBxH4gRL1ovrmyDmI2zqIivqDCE",
+    authDomain: "todolist-7afba.firebaseapp.com",
+    databaseURL: "https://todolist-7afba.firebaseio.com",
+    storageBucket: "todolist-7afba.appspot.com",
+    messagingSenderId: "289641993910"
+  };
 
 @NgModule({
   declarations: [
@@ -12,10 +25,12 @@ import { TabsPage } from '../pages/tabs/tabs';
     AboutPage,
     ContactPage,
     HomePage,
-    TabsPage
+    TabsPage,
+    DetailsPage
   ],
   imports: [
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    AngularFireModule.initializeApp(firebaseConfig)
   ],
   bootstrap: [IonicApp],
   entryComponents: [
@@ -23,8 +38,10 @@ import { TabsPage } from '../pages/tabs/tabs';
     AboutPage,
     ContactPage,
     HomePage,
-    TabsPage
+    TabsPage,
+    DetailsPage
   ],
   providers: [{provide: ErrorHandler, useClass: IonicErrorHandler}]
 })
 export class AppModule {}
+
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,15 +1,37 @@
 import { Component } from '@angular/core';
 
-import { NavController } from 'ionic-angular';
+import { NavController, AlertController } from 'ionic-angular';
+
+import { AngularFire, FirebaseListObservable } from 'angularfire2';
+
+// get a reference to the details page
+import { DetailsPage } from '../details/details';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  // PROPERTIES
+  todos: FirebaseListObservable<any>;
 
-  constructor(public navCtrl: NavController) {
-
+  // CONSTRUCTOR
+  constructor(
+    public navCtrl: NavController,
+    public alertCtrl: AlertController,
+    af: AngularFire) {
+    this.todos = af.database.list('/todos');
+  }
+  // METHODS
+  addTodo() {
+    this.navCtrl.push(DetailsPage);
   }
 
+  editTodo(){
+    this.navCtrl.push(DetailsPage);
+  }
+
+  deletGame(todoID){
+    this.todos.remove(todoID);
+  }
 }
